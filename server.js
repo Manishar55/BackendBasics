fe0ca46002c6436475bf34148d42a77529f8b043
Manisha Rana <manisharana02005@gmail.com>
2025-03-30 23:50:58 +0530

    Initialized a backend app

---
+++ server.js
@@ -0,0 +1,9 @@
+
+//created instance of express
+const express = require('express');
+const app = express(); //named app of the instance
+
+app.listen(3000, ()=>{
+    console.log('server started at port no. 3000');
+    
+});
