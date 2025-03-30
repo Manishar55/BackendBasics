9336ff7e1a759edd99319d8bc66b739f7e10a1ea
Manisha Rana <manisharana02005@gmail.com>
2025-03-31 01:01:51 +0530

    POST request successfully using postman

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 
+//server instantiate
 //created instance of express
 const express = require('express');
 const app = express(); //named app of the instance
 
-app.listen(3000, ()=>{
+//used to parse req.body in express -> PUT or POST
+const bodyParser=require('body-parser');
+
+//specifically parse JSON data & add it to the request.body object
+app.use(bodyParser.json());
+
+
+//activate the server on 3000 port 
+app.listen(3000, ()=>{ 
     console.log('server started at port no. 3000');
-    
 });
+
+//Routes
+app.get('/', (req, res)=>{
+    res.send("Hello Everyone");
+});
+
+
+app.post('/api/cars', (req, res)=>{
+    const {name, brand} = req.body;
+    console.log(name);
+    console.log(brand);
+    
+    res.send('car submitted successfully');
+})
+
