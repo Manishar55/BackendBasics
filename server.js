e5f34c9a36bda0ed7f716810486a572745e7dc98
Manisha Rana <manisharana02005@gmail.com>
2025-04-06 12:33:20 +0530

    Updated notes

--- server.js
+++ server.js
@@ -4,14 +4,16 @@
 const express = require('express');
 const app = express(); //named app of the instance
 
+
 //used to parse req.body in express -> PUT or POST
 const bodyParser=require('body-parser');
+
 
 //specifically parse JSON data & add it to the request.body object
 app.use(bodyParser.json());
 
 
-//activate the server on 3000 port 
+//activate the server on 3000 port  
 app.listen(3000, ()=>{ 
     console.log('server started at port no. 3000');
 });
@@ -30,7 +32,9 @@ app.post('/api/cars', (req, res)=>{
     res.send('car submitted successfully');
 });
 
+
 const mongoose=require('mongoose'); //created instance of mongoose
+
 
 //connected mongoDB with server using mongoose
 mongoose.connect('mongodb://localhost:27017/myDatabase', {
